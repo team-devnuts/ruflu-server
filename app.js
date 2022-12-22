4ec3c0f5aa2bc6b20a83830c08e9f54019a94985
dustjq1004 <dustjq1005@naver.com>
2022-12-23 00:32:22 +0900

    refactor/#5 [refactor] : Ruflu Server 초기 설정 - 환경변수 설정을 위한 dotenv 사용 DB 연결 config 파일 코드 수정

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
-const http = require('node:http');
 const app = express();
+const http = require('node:http');
+const server = http.createServer(app);
 const bodyParser = require('body-parser');
-const db_config = require(__dirname + '/config/database.js');
+const dbConfig = require(__dirname + '/config/database.js');
 const cookieParser = require('cookie-parser');
 const socketIO = require('socket.io');
-const server = http.createServer(app);
 const io = socketIO(server);
+
+// dotenv 환경변수 설정 require
+require("dotenv").config();
 
 // express 서버 포트 설정
 app.set('port', 8005);
@@ -57,11 +60,20 @@ app.use('/login', loginRouter);
 // 채팅 관련 api
 app.use('/chat', chatRouter);
 app.use(cookieParser());
-
 app.use(express.static('public'));
 app.use(express.static('uploads'));
+
+// DB 연결
+dbConfig({
+    host: process.env.MYSQL_ACCESS_HOST,
+    port: process.env.MYSQL_ACCESS_PORT,
+    user: process.env.MYSQL_ACCESS_ID,
+    password: process.env.MYSQL_ACCESS_PW,
+    database: process.env.MYSQL_DATABASE
+})
 
 // 서버 생성
 server.listen(app.get('port'), function(){
     console.log('Express server listening on port ' + app.get('port'));
 });
+
