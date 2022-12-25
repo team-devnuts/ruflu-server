d7e6baca0aa3c40c58fc37175fd5a3c86ea6506c
dustjq1004 <dustjq1005@naver.com>
2022-12-25 23:27:02 +0900

    refactor/#5 [refactor] : 이미지 업로드 디렉토리 경로 환경셜정에서 가져오도록 변경

--- app.js
+++ app.js
@@ -8,11 +8,40 @@ const cookieParser = require('cookie-parser');
 const socketIO = require('socket.io');
 const io = socketIO(server);
 
-// dotenv 환경변수 설정 require
+/* dotenv 환경변수 설정 require */
+// dotenv 사용하여 서버주소, 고유 API KEY등 민감한 정보를 보안 관리한다.
 require("dotenv").config();
 
-// express 서버 포트 설정
-app.set('port', 8005);
+const rufluRouter = require('./routes/ruflu/ruflu');
+const mainRouter = require('./routes/main/main');
+const alarmRouter = require('./routes/alarm/alarm');
+const loginRouter = require('./routes/login/ouathAPI');
+const chatRouter = require('./routes/chat/chat');
+//const require  = require('app-root-path');
+
+//app.use(bodyParser.json);
+app.use(express.urlencoded({extended : false}));
+app.use(express.json());
+app.use('/home', rufluRouter);
+app.use('/main', mainRouter);
+app.use('/alarm', alarmRouter);
+app.use('/login', loginRouter);
+// 채팅 관련 api
+app.use('/chat', chatRouter);
+app.use(cookieParser());
+app.use(express.static('public'));
+app.use(express.static(process.env.IMAGE_DIR));
+
+// DB 연결
+dbConfig({
+    host: process.env.MYSQL_ACCESS_HOST,
+    port: process.env.MYSQL_ACCESS_PORT,
+    user: process.env.MYSQL_ACCESS_ID,
+    password: process.env.MYSQL_ACCESS_PW,
+    database: process.env.MYSQL_DATABASE
+})
+
+app.set('port', process.env.APP_PORT || '3000')
 
 app.get("/", function(req,res) {
     res.json({state:200});
@@ -42,37 +71,7 @@ io.on("connection", (socket) => {
     });
 });
 
-
-const rufluRouter = require('./routes/ruflu/ruflu');
-const mainRouter = require('./routes/main/main');
-const alarmRouter = require('./routes/alarm/alarm');
-const loginRouter = require('./routes/login/ouathAPI');
-const chatRouter = require('./routes/chat/chat');
-//const require  = require('app-root-path');
-
-//app.use(bodyParser.json);
-app.use(express.urlencoded({extended : false}));
-app.use(express.json());
-app.use('/home', rufluRouter);
-app.use('/main', mainRouter);
-app.use('/alarm', alarmRouter);
-app.use('/login', loginRouter);
-// 채팅 관련 api
-app.use('/chat', chatRouter);
-app.use(cookieParser());
-app.use(express.static('public'));
-app.use(express.static('uploads'));
-
-// DB 연결
-dbConfig({
-    host: process.env.MYSQL_ACCESS_HOST,
-    port: process.env.MYSQL_ACCESS_PORT,
-    user: process.env.MYSQL_ACCESS_ID,
-    password: process.env.MYSQL_ACCESS_PW,
-    database: process.env.MYSQL_DATABASE
-})
-
-// 서버 생성
+// 서버 open
 server.listen(app.get('port'), function(){
     console.log('Express server listening on port ' + app.get('port'));
 });
