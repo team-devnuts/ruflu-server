bb261dc054dfde975ff1289f5eaa8d59e2f362b4
dustjq1004 <dustjq1005@naver.com>
2023-04-02 21:58:11 +0900

    #13 : jwt 오류 및 기능 수정 dquote> 1. Access Token 발행 시 Refresh Token 으로 검증 후 decoded 안에 있는 유저 정버로 발행 dquote> 2. 검증시 scretkey 파라미터로 받아서 처리 3. 검증 결과 user 정보 반환 추가

--- src/gateways/jwt.js
+++ src/gateways/jwt.js
@@ -1,5 +1,7 @@
 const { verify } = require("crypto");
 const jsonwebtoken = require("jsonwebtoken");
+const config = require("../config");
+const ClientException = require("../exception/client-exception");
 
 module.exports = {
     sign: (user) => {
@@ -23,11 +25,10 @@ module.exports = {
         }
         return result;
     }
-    ,publishAccessToken: (user) => {
-        const payload = {// 페이로드 유저 정보를 담아서 보냄.
-            id: user.user_id,
-            phoneNumber: user.phone_number,
-        }
+    ,publishAccessToken: (refreshToken) => {
+        const validationResult = verify(refreshToken, config.jwtRefreshSecretKey);
+        if(!validationResult.ok) new ClientException(403, "검증 되지 않은 토큰");
+
         const result = {// payload, secretkey(사용자 임의로 생성), options
             accessToken : jsonwebtoken.sign(payload, process.env.JWT_SECRETKEY
                     ,{
@@ -40,28 +41,29 @@ module.exports = {
     }
     ,publishRefreshToken: (user) => {
         const payload = {// 페이로드 유저 정보를 담아서 보냄.
-            id: user.user_id,
+            userId: user.user_id,
             phoneNumber: user.phone_number,
         }
-        const result = {// payload, secretkey(사용자 임의로 생성), options
-            refreshToken : jsonwebtoken.sign(payload, process.env.JWT_REFRESH_SECRETKEY
+        
+        const result = // payload, secretkey(사용자 임의로 생성), options
+            jsonwebtoken.sign(payload, process.env.JWT_REFRESH_SECRETKEY
                 ,{
                     algorithm: 'HS256',     // 암호화 알고리즘
                     expiresIn: '30d', 	    // 유효기간
                 }
             )
-        }
         return result;
     },
-    verify: (token) => {
+    verify: (token, secretkey) => {
         let decoded = null;
         try {
-        decoded = jwt.verify(token, secret);
-        return {
-            ok: true,
-            id: decoded.id,
-            role: decoded.role,
-        };
+            decoded = jsonwebtoken.verify(token, secretkey);
+
+            return {
+                ok: true,
+                user : {userId : decoded.userId,
+                        phoneNumber : decoded.phoneNumber}
+            };
         } catch (err) {
             return {
                 ok: false,
