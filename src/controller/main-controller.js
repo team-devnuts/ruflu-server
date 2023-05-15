ca3cf6009c4cdc400bb973a234ae0e2a6225878c
dustjq1004 <dustjq1005@naver.com>
2023-05-15 20:35:15 +0900

    #5 : eslint controller 적용 1. 사용하지 않는 변수 제거 2. 카멜 케이스 변수 수정 3. return value await 수정

--- src/controller/main-controller.js
+++ src/controller/main-controller.js
@@ -6,19 +6,19 @@ const updateLocation = async (req, res) => {
     const userId = req.get("user_id");
     const data = {latitude,longitude,"user_id":userId};
     req.responseObject.result = await mainService.updateLocation(data);
-    return req.responseObject;
+    res.json(req.responseObject);
 };
 
 const updateToken = async (req, res) => {
     const {token} = req.body;
     const data = {"token": token, "user_id": req.get("user_id")};
     req.responseObject.result = await mainService.updateToken(data);
-    return req.responseObject;
+    res.json(req.responseObject);
 };
 
 const getUserDetail = async (req, res) => {
     req.responseObject.result = await userService.getUserDetail({"user_id": req.params.user_id});
-    return req.responseObject;
+    res.json(req.responseObject);
 };
 
 module.exports = {
