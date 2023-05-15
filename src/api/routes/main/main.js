7a8d0acee3a1cc0cf10e0e0fa2ed078fc7046239
dustjq1004 <dustjq1005@naver.com>
2023-05-15 20:16:45 +0900

    1. 사용하지 않는 변수 삭제

--- src/api/routes/main/main.js
+++ src/api/routes/main/main.js
@@ -1,20 +1,21 @@
 const express = require('express');
-const router = express.Router();
-const controller = require('../../../controller/mainController');
 
-module.exports = (app, verifyToken) => {
+const router = express.Router();
+const controller = require('../../../controller/main-controller');
+
+module.exports = (app) => {
     app.use('/main', router);
     
     // 라우터의 get 함수 를 이용해 request url에 대한 업무처리 로직 정의
-    router.get('/', function(req, res, next) {
+    router.get('/', (req, res) => {
         res.json({state:200});
     });
     
-    router.post("/location", async (req, res, next) => {
+    router.post("/location", async (req, res) => {
         res.json(await controller.updateLocation(req, res));
     });
 
-    router.post("/alarm", async (req, res, next) => {
+    router.post("/alarm", async (req, res) => {
         res.json(await controller.updateToken(req, res));
     }); 
         
