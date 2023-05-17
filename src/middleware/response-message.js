76e1c9fb2961aacda57f5a9b33ac613bdab3eec9
dustjq1004 <dustjq1005@naver.com>
2023-05-17 19:40:40 +0900

    #5 : eslint middleware 적용 1. exception-handler 2. jwt-verity 3. response-message

--- src/middleware/response-message.js
+++ src/middleware/response-message.js
@@ -1,11 +1,9 @@
-'use strict';
+
 
 const logger = require("../loaders/logger");
 
-module.exports = () => {
-    return function (req, res, next) {
-        logger.debug(`init response message middleWare : ${new Date()}`);
-        req.responseObject = {"code": 200, "message": "", "result" : {}};
-        next();
-    }
-}
+module.exports = () => function (req, res, next) {
+    logger.debug(`init response message middleWare : ${new Date()}`);
+    req.responseObject = {"code": 200, "message": "", "result" : {}};
+    next();
+ }
